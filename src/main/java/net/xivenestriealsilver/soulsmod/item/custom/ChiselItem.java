2d24744ee92a42739e8b17bda3a85d5ca5992406
Xivenestrieal <nattynjita@gmail.com>
2025-12-07 21:56:40 +0100

    Added lazurite key crafting recipe and place holder; In the process of making Chisel Item Game functional, no errors;

---
+++ src/main/java/net/xivenestriealsilver/soulsmod/item/custom/ChiselItem.java
@@ -0,0 +1,19 @@
+package net.xivenestriealsilver.soulsmod.item.custom;
+
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+
+public class ChiselItem extends Item{
+    public ChiselItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        Level level = context.getLevel()
+
+        return super.useOn(context);
+    }
+}
---
+++ src/main/java/net/xivenestriealsilver/soulsmod/item/custom/HogyokuItem.java
@@ -0,0 +1,11 @@
+package net.xivenestriealsilver.soulsmod.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class HogyokuItem extends Item{
+    public HogyokuItem(Properties properties) {
+        super(properties);
+    }
+
+
+}
