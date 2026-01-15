1c96c74c1b072b6472035a74762057801ebb9233
Xivenestrieal <nattynjita@gmail.com>
2026-01-15 15:16:23 +0100

    Done with ModBlockLootTableProvider; started om DataGenerators;

--- src/main/java/net/xivenestriealsilver/soulsmod/datagen/DataGenerators.java
+++ src/main/java/net/xivenestriealsilver/soulsmod/datagen/DataGenerators.java
@@ -1,4 +1,15 @@
 package net.xivenestriealsilver.soulsmod.datagen;
 
+import net.minecraft.data.DataGenerator;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.xivenestriealsilver.soulsmod.SoulsMod;
+
+@EventBusSubscriber(modid = SoulsMod.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+    }
 }
--- src/main/java/net/xivenestriealsilver/soulsmod/datagen/ModBlockLootTableProvider.java
+++ src/main/java/net/xivenestriealsilver/soulsmod/datagen/ModBlockLootTableProvider.java
@@ -1,11 +1,21 @@
 package net.xivenestriealsilver.soulsmod.datagen;
 
-import net.minecraft.core.Holder;
 import net.xivenestriealsilver.soulsmod.block.ModBlocks;
+import net.xivenestriealsilver.soulsmod.item.ModItems;
+import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 
 import java.util.Set;
 
@@ -16,7 +26,22 @@ public class ModBlockLootTableProvider extends BlockLootSubProvider {
 
     @Override
     protected void generate() {
+        dropSelf(ModBlocks.LAZURITE_BLOCK.get());
+        // dropSelf(ModBlocks.SEEKING_BLOCK.get());
 
+        add(ModBlocks.LAZURITE_ORE.get(),
+                block -> createOreDrop(ModBlocks.LAZURITE_ORE.get(), ModItems.RAW_LAZURITE.get()));
+        add(ModBlocks.DEEPSLATE_LAZURITE_ORE.get(),
+                block -> createMultipleOreDrops(ModBlocks.DEEPSLATE_LAZURITE_ORE.get(), ModItems.RAW_LAZURITE.get(), 1, 3));
+
+    }
+
+    protected LootTable.Builder createMultipleOreDrops(Block pBlock, Item item, float minDrops, float maxDrops) {
+        HolderLookup.RegistryLookup<Enchantment> registrylookup = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
+        return this.createSilkTouchDispatchTable(pBlock,
+                this.applyExplosionDecay(pBlock, LootItem.lootTableItem(item)
+                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(minDrops, maxDrops)))
+                        .apply(ApplyBonusCount.addOreBonusCount(registrylookup.getOrThrow(Enchantments.FORTUNE)))));
     }
 
     @Override
