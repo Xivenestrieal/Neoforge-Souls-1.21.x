93e18dda36f45d2257416199bd27b699a640fcf4
Xivenestrieal <nattynjita@gmail.com>
2026-02-17 17:48:20 +0100

    Done with Data generators; Removed Everything in data; Functionable?; done with mod block sate provider; Tags done?;

--- src/main/java/net/xivenestriealsilver/soulsmod/datagen/ModBlockStateProvider.java
+++ src/main/java/net/xivenestriealsilver/soulsmod/datagen/ModBlockStateProvider.java
@@ -1,4 +1,17 @@
 package net.xivenestriealsilver.soulsmod.datagen;
 
-public class ModBlockStateProvider {
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.xivenestriealsilver.soulsmod.SoulsMod;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, SoulsMod.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+
+    }
 }
